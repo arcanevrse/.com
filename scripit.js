7770748bd65239f7db29ff7928f9db8de589b89b
arcanevrse <sgtjulietnaboya54321@gmail.com>
2026-03-24 20:45:08 +0100

    finished home section

--- scripit.js
+++ scripit.js
@@ -1,6 +1,12 @@
 const nav = document.getElementById("btn");
 const menu = document.getElementById("nav2menu");
 
-nav.addEventListener("click", () => {
+nav.addEventListener("click", (e) => {
+  e.stopPropagation();
   menu.classList.toggle("active");
+  document.addEventListener("click", (e) => {
+    if (!menu.contains(e.target)) {
+      menu.classList.remove("active");
+    }
+  });
 });
